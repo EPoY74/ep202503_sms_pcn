db2fd626cea77f0a02cd5bb9836cc304049db02c
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-03-09 18:44:39 +0500

    Запустил.РАботает

--- ep202503_sms_pcn/sms_server.py
+++ ep202503_sms_pcn/sms_server.py
@@ -1,19 +1,26 @@
 """
 Слушает модем на сом порту
 """
-from time import sleep
-import serial
-from serial import Serial
 import asyncio
 
+import serial
+from serial import Serial
 
-port = "COM"
+port = "COM9"
 baudrate = 9600
 
 try:
     modem: Serial = serial.Serial(port, baudrate, timeout=1)
 except serial.SerialException as err:
-    raise ValueError(f"Не удалось открыть COM-порт: {err}")
+    raise ValueError(f"Не удалось открыть COM-порт: {err}") from err
+
+
+async def decode_resp(response: bytes | None ):
+    if response:
+        decoded_response = response.decode(encoding = "utf-8", errors = "strict")
+        print(f"Ответ модема: {decoded_response}")
+        await asyncio.sleep(0.05)
+
 
 async def listen_to_modem():
     """
@@ -21,8 +28,21 @@ async def listen_to_modem():
     """
     while True:
         if (modem is not None) and (modem.in_waiting >= 1):
-            response = modem.read_all()
-            if response:
-                decoded_response = response.decode(encoding = "utf-8", errors = "strict")
-                print(f"Ответ модема: {decoded_response}")
-                await asyncio.sleep(0.05)
+            response: bytes | None = modem.read_all()
+            await decode_resp(response)
+
+    
+async def main():
+    """
+    основная асинхронная функция    
+    """
+    await listen_to_modem()
+
+
+# запуск асинхронного цикла
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    print("Программа завершена")
+finally:
+    modem.close()
