ee3c15366aec99ffbab14b12358e5b04f14eb609
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-03-09 16:19:53 +0500

    Получаю все сообщения из модема

--- ep202503_sms_pcn/read_sms.py
+++ ep202503_sms_pcn/read_sms.py
@@ -1,18 +1,22 @@
 """
 Читаю смс сообщения из модема
 """
+
 import time
 
 import serial
 from serial import Serial
 
-
-PORT: str = "COM10"
+PORT: str = "COM13"
 BAUDRATE = 9600
 
 modem: Serial = serial.Serial(PORT, BAUDRATE, timeout=1)
 
-def send_at_commanf(command, wait_time = 1):
+
+def send_at_command(command, wait_time=1) -> str | None:
+    """
+    Пишет команды в модем и возврващаем ответ
+    """
     modem.write((command + "\r\n").encode())
     time.sleep(wait_time)
     response = modem.read_all()
@@ -21,4 +25,29 @@ def send_at_commanf(command, wait_time = 1):
         return decoded_dresponse
     else:
         print("Ответ не получен")
-        return None 
+        return None
+
+
+def read_all_sms():
+    """
+    Читает все смс из модема
+    """
+    send_at_command("AT+CMGF=1")
+    return send_at_command("AT+CMGL=\"ALL\"") #AT+CMGR 
+
+
+def check_modem_resp():
+    return send_at_command("AT")
+        
+
+def main():
+    """
+    Основное тело программы
+    """
+    # print(check_modem_resp())
+    print(read_all_sms())
+    # print(send_at_command("AT+CLAC")) # выводит все поддерживаемые команды
+    
+
+if __name__ == "__main__":
+    main()
