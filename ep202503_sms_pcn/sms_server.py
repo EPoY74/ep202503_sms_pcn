1b34a56cded4b40089c6c1a3131e340e5b369e38
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-03-10 09:29:17 +0500

    Добавил постоянную попытку подключения при недоступности пора

--- ep202503_sms_pcn/sms_server.py
+++ ep202503_sms_pcn/sms_server.py
@@ -2,44 +2,77 @@
 Слушает модем на сом порту
 """
 import asyncio
-#from sqlite3 import SQLITE_ERROR_MISSING_COLLSEQ
+from datetime import datetime
+import time
 
 import serial
 from serial import Serial
 
 port = "COM9"
 baudrate = 115200
+modem: Serial | None= None
 
-try:
-    modem: Serial = serial.Serial(port, baudrate, timeout=1)
-except serial.SerialException as err:
-    raise ValueError(f"Не удалось открыть COM-порт: {err}") from err
+
+def now_datetime():
+    """
+    Возворащает текущую дату-время в формате ("%Y-%m-%d %H:%M:%S")
+    """
+    datetime_now = datetime.now()
+    return datetime_now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+while True:
+    try:
+        modem = serial.Serial(port, baudrate, timeout=1)
+        print(now_datetime(), f"Подключение успешно по {port}")
+        break
+    except serial.SerialException as err:
+        print(now_datetime(), f"Подключение не успешно по {port}")
+        time.sleep(10)
+
+
+
 
 
 async def decode_resp(response: bytes | None ):
     if response:
-        decoded_response = response.decode(encoding = "utf-8", errors = "strict")
-        print(f"Ответ модема: {decoded_response}")
+        decoded_response =(
+            response.decode(
+                encoding = "utf-8",
+                  errors = "strict").strip("\r\n^")
+        )
+        formatted_datetime = now_datetime()
+        print(f"{formatted_datetime}: {decoded_response}")
         await asyncio.sleep(0.1)
         if decoded_response:
             return decoded_response
         else:
             return None
+        
+
+async def sms_delete(sms_index):
+    """
+    Удаляет смс с номером sms_index
+    """
+    await send_at_command("AT+CMGF=1")
+    del_sms = "AT+CMGD=" + str(sms_index)
+    print(f"Deleting {sms_index} SMS", await send_at_command(del_sms))
 
 
 async def send_at_command(command, wait_time=1) -> str | None:
     """
     Пишет команды в модем и возврващаем ответ
     """
-    modem.write((command + "\r\n").encode())
-    await asyncio.sleep(wait_time)
-    response = modem.read_all()
-    if response is not None:
-        decoded_dresponse = response.decode(encoding = "utf-8", errors = "strict")
-        return decoded_dresponse
-    else:
-        print("Ответ не получен")
-        return None
+    if modem:
+        modem.write((command + "\r\n").encode())
+        await asyncio.sleep(wait_time)
+        response = modem.read_all()
+        if response is not None:
+            decoded_dresponse = response.decode(encoding = "utf-8", errors = "strict")
+            return decoded_dresponse
+        else:
+            print("Ответ не получен")
+            return None
 
 
 async def read_sms(sms_index_: int):
@@ -55,9 +88,10 @@ async def read_sms(sms_index_: int):
 async def sms_split(sms_text_: str):
     sms_clean_ = sms_text_.strip().split("+CMGR: ")[1:]
     sms_parts_ = sms_clean_  #.split(",", 4)
-    sms_status_ = sms_parts_[0].strip("\"")
-    sms_inp_number_ = sms_parts_[1].strip("\"")
-    sms_datetime_ = sms_parts_[3].strip("\"")
+    print(sms_parts_)
+    sms_status_ = sms_parts_[0].strip('"')
+    sms_inp_number_ = sms_parts_[1].strip('"')
+    sms_datetime_ = sms_parts_[3].strip('"')
     sms_text_ = sms_parts_[4].split("\r\n")[1]
 
     print(sms_status_)
@@ -79,7 +113,8 @@ async def listen_to_modem():
                 print(f"Получено  уведомление о новой СМС с номером {sms_index}")
 
                 sms_content = await read_sms(sms_index)
-                print(f"Содержимое СМС: {sms_content}")
+                print(f"Содержимое СМС: {sms_content}") 
+                await sms_delete(sms_index)
 
                 if sms_content:
                     await sms_split(sms_content)
@@ -99,4 +134,5 @@ try:
 except KeyboardInterrupt:
     print("Программа завершена")
 finally:
-    modem.close()
+    if modem:
+        modem.close()
